d824172908500fa85851f549e23cd61863d749b6
BunnyBlue <bunnyblueair@gmail.com>
2015-03-28 13:54:54 +0800

    add  plugin demo src

---
+++ Plugins/appcenter/src/com/taobao/android/gamecenter/main/ActivityInternalActivity.java
@@ -0,0 +1,17 @@
+package com.taobao.android.gamecenter.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityInternalActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+	}
+
+
+
+
+}
