46bcbff31a2302cc513cb7415fb2d5fd53d36cf7
BunnyBlue <bunnyblueair@gmail.com>
2015-04-09 17:40:50 +0800

    update lic

--- src/blue/stack/openAtlas/android/lifecycle/PanguActivity.java
+++ src/blue/stack/openAtlas/android/lifecycle/PanguActivity.java
@@ -2,7 +2,7 @@
  *  OpenAtlasForAndroid Project
 The MIT License (MIT) Copyright (AwbDebug) 2015 Bunny Blue,achellies
 
-Permission is hereby granted, free of charge, to any person obtaining mApp copy of this software
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
 and associated documentation files (the "Software"), to deal in the Software 
 without restriction, including without limitation the rights to use, copy, modify, 
 merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
