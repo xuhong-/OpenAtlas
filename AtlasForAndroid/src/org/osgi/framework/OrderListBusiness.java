ea44b5f6e8afb2d9f2e6f47027003229aa6c2223
BunnyBlue <bunnyblueair@gmail.com>
2015-05-13 10:37:32 +0800

    update osgi

--- AtlasForAndroid/src/org/osgi/framework/OrderListBusiness.java
+++
@@ -1,9 +0,0 @@
-package org.osgi.framework;
-
-public class OrderListBusiness {
-    public static final String CURRENT_PAGE_KEY = "page";
-    public static final int PAGE_SIZE = 12;
-    public static final String PAGE_SIZE_KEY = "pageSize";
-    public static final String RESULT_LIST_KEY = "cell";
-    public static final String TOTAL_NUM_KEY = "total";
-}
